cb4c21a6b0d46236ce682ba2021e2424399248c3
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2026-02-09 01:05:09 +0100

    Remove unused code

--- main.cpp
+++ main.cpp
@@ -1,22 +1,5 @@
 #include <SDK/foobar2000.h>
 
-class library_enumerator : public initquit {
-public:
-    virtual ~library_enumerator() = default;
-
-    void on_init() override {
-        metadb_handle_list items;
-        static_api_ptr_t<library_manager>()->get_all_items(items);
-
-        for (t_size i = 0; i < items.get_count(); ++i) {
-            auto* path = items[i]->get_path();
-            console::print(path);
-        }
-    }
-};
-
-static initquit_factory_t<library_enumerator> g_library_enumerator;
-
 DECLARE_COMPONENT_VERSION(
   "foo_ai",
   "0.0.1",
