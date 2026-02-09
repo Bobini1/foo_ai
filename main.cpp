8cdae9fb7108e85aa0095124c5c3061d141cae56
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2026-02-09 18:01:47 +0100

    Change name

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <SDK/foobar2000.h>
 
 DECLARE_COMPONENT_VERSION(
-  "foo_ai",
+  "AI MCP Server",
   "1.0.0",
   "AI MCP control for foobar2000.\n"
   "Control your music player with an AI assistant!"
