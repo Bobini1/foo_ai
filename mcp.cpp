fb3806f452f3b767c15efa152d08bd21fd98f244
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2026-02-10 12:52:21 +0100

    Allow sorting

--- mcp.cpp
+++ mcp.cpp
@@ -326,8 +326,8 @@ static result handle_tracks(search_index::ptr items, const int limit, const int 
         auto mgr = search_filter_manager_v2::get();
         search_filter_v2::ptr filter = mgr->create_ex(query.c_str(),
                                                       nullptr,
-                                                      search_filter_manager_v2::KFlagSuppressNotify);
-        const auto res = items->search(filter, nullptr, 0, fb2k::noAbort);
+                                                      search_filter_manager_v2::KFlagAllowSort | search_filter_manager_v2::KFlagSuppressNotify);
+        const auto res = items->search(filter, nullptr, search_index::flag_sort, fb2k::noAbort);
         list = res->as_list_of<metadb_handle>();
     }
     catch (std::exception& e)
